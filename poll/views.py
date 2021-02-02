759a59bb6b984a5d64e42a0b814ee0ee48221762
Lakeram Narine <nar19002@byui.edu>
2021-02-02 08:03:21 -0700

    initial commit

---
+++ poll/views.py
@@ -0,0 +1,15 @@
+
+'''
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")'''
+
+from django.shortcuts import render
+
+# Create your views here.
+def page(request):
+    
+    return render(request,"index.html")
+
