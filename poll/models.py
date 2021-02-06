70a9bab1ee915eca07dfdc3c6ff3a3ada1d09137
Yu-Chun <dyj0183@byui.edu>
2021-02-06 13:54:44 -0700

    database created

--- poll/models.py
+++ poll/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+class Teacher(models.Model):
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+
 class Question(models.Model):
     question = models.CharField(max_length=128)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
-
-class Teacher(models.Model):
-    firstName = models.CharField(max_length=32)
-    lastName = models.CharField(max_length=32)
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=128)
+    votes = models.IntegerField() 
    
