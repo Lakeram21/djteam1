f8f22dfa3e71d7c9d9ad940063ad9b895facebed
Lakeram Narine <nar19002@byui.edu>
2021-02-06 13:26:05 -0700

    change

--- poll/models.py
+++ poll/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-Class Question(models.Model):
+class Question(models.Model):
     question = models.CharField(max_length=128)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+
+class Teacher(models.Model):
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+   
