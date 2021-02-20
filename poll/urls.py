ca26ebeb53b992acce5e27f77f418d9881d9d50a
Conner <dre19002@byui.edu>
2021-02-20 13:44:07 -0700

    worked on some templates and views

--- poll/urls.py
+++ poll/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'poll'
 urlpatterns = [
-    path('polls/<int:poll_num>', views.poll)
+    path('', views.home),
+    path('polls/', views.index, name='index'),
+    path('polls/<int:poll_num>', views.poll, name='poll'),
+    path('polls/<int:poll_num>/results', views.results, name='results')
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,31 +1,40 @@
-
-'''
 from django.http import HttpResponse, HttpResponseRedirect
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")'''
-
 from django.shortcuts import render
+
 from .models import Teacher, Question, Answer # using the database
 
+def home(request):
+    return render(request, "home.html")
 
-def page(request):
+def index(request):
     #grabbing all the questions
-    teacher = Teacher.objects.all() #getting all the information in questio
+    teachers = Teacher.objects.all() #getting all the information in questio
     #however i could use an .order_by("type of order") to grab a specifc question.
-    question = Question.objects.all()
+    questions = Question.objects.all()
     answers = Answer.objects.all()
-    return render(request,"index.html", {"teachDict":teacher, "quest":question, "answers": answers}) # attaching the data to the html with a dictionary and we do this using a template context)
+    return render(request, "index.html", {
+        "teachers": teachers,
+        "questions": questions,
+        "answers": answers}) # attaching the data to the html with a dictionary and we do this using a template context)
 
+def poll(request, question_num):
+    if request.method == "POST":
+        # Process POST by incrementing the votes in the corresponding answer by 1 and redirect to the results page
+        pass
+    else: # GET
+        q = Question.object.get(question_num=question_num)
+        a = list(Answer.object.filter(question=q))
+        return render(request, 'poll_detail.html', {'q': q, 'answers': a})
+
+def results(request, question_num):
+    q = Question.object.get(question_num=question_num)
+    a = list(Answer.object.filter(question=q))
+    return render(request, 'poll_results.html', {'q': q, 'answers': a})
+
+"""
 def vote(request):
     vote = request.POST.get('vote')
     vote.votes += 1
     votes.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    
-def poll(request, question_num):
-    q = Question.object.get(question_num=question_num)
-    a = list(Answer.object.filter(question=q))
-    return render(request, 'index.html', {'question': q, 'answers': a})
-
+"""
